94d1539d3453a57a021743e61b9f696287dd6656
leonseki <leonstokyo@gmail.com>
2024-04-10 22:24:49 +0900

    api日志完成

--- zeus-common-api/src/main/java/jp/tokyo/leon/zeus/common/log/EnableLogResolver.java
+++ zeus-common-api/src/main/java/jp/tokyo/leon/zeus/common/log/EnableLogResolver.java
@@ -1,24 +1,44 @@
 package jp.tokyo.leon.zeus.common.log;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.stereotype.Component;
+import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
+import org.springframework.core.type.filter.AnnotationTypeFilter;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * @author leon
  * @date 2024/4/9 22:28
  */
-@Component
 public class EnableLogResolver {
+    public Set<String> findClassesWithAnnotation(Class<? extends Annotation> annotation) {
+        ClassPathScanningCandidateComponentProvider scanner = new ClassPathScanningCandidateComponentProvider(false);
+        scanner.addIncludeFilter(new AnnotationTypeFilter(annotation));
 
-    private final ApplicationContext applicationContext;
-
-    public EnableLogResolver(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
+        Set<String> annotatedClasses = new HashSet<>();
+        for (org.springframework.beans.factory.config.BeanDefinition beanDefinition : scanner.findCandidateComponents("")) {
+            try {
+                Class<?> clazz = Class.forName(beanDefinition.getBeanClassName());
+                System.out.println(clazz.getName());
+                Annotation ann = clazz.getAnnotation(annotation);
+                if (ann != null) {
+                    Method method = ann.annotationType().getMethod("value");
+                    String[] value = (String[]) method.invoke(ann);
+                    Set<String> collect = Arrays.stream(value).collect(Collectors.toSet());
+                    annotatedClasses.addAll(collect);
+                }
+            } catch (Exception e) {
+                // 处理异常
+                e.printStackTrace();
+            }
+        }
+        return annotatedClasses;
     }
+
 
     public boolean shouldEnableLogging(EnableZeusApiLog enableLog, Class<?> targetClass) {
         Set<String> basePackages = new HashSet<>(Arrays.asList(enableLog.basePackages()));
--- zeus-common-api/src/main/java/jp/tokyo/leon/zeus/common/log/ZeusApiLogAspect.java
+++ zeus-common-api/src/main/java/jp/tokyo/leon/zeus/common/log/ZeusApiLogAspect.java
@@ -1,5 +1,6 @@
 package jp.tokyo.leon.zeus.common.log;
 
+import jakarta.annotation.PostConstruct;
 import jakarta.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -8,38 +9,56 @@ import org.aspectj.lang.annotation.Pointcut;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.lang.reflect.Method;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 /**
  * @author leon
  * @date 2024/4/9 18:59
  */
 @Aspect
-@Component
 public class ZeusApiLogAspect {
+
+    private final EnableLogResolver enableLogResolver;
+
+    private Set<String> scannedPackage = new HashSet<>();
+
+    @PostConstruct
+    public void init() {
+        scannedPackage = enableLogResolver.findClassesWithAnnotation(EnableZeusApiLog.class);
+    }
+
+
+    public ZeusApiLogAspect(EnableLogResolver enableLogResolver) {
+        this.enableLogResolver = enableLogResolver;
+    }
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 
 
-    @Pointcut("within(jp.tokyo.leon.zeus)")
+    @Pointcut("@within(org.springframework.web.bind.annotation.RestController)")
     public void controllerPointcut() {
     }
 
-    @Around("logPointCut()")
+    @Around("controllerPointcut()")
     public Object recordLog(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        // 先获取方法注解，没有日志注解不记录日志
         MethodSignature signature = (MethodSignature) proceedingJoinPoint.getSignature();
         Method method = signature.getMethod();
+        if (!scannedPackage.isEmpty() && !isPackageOrSubpackage(scannedPackage, method.getDeclaringClass().getName())) {
+            return null;
+        }
+
+        System.out.println(method.getDeclaringClass().getName());
         ZeusApiLog annotation = method.getAnnotation(ZeusApiLog.class);
-        if (Objects.isNull(annotation)) {
-            return proceedingJoinPoint.proceed(proceedingJoinPoint.getArgs());
+        String description = "";
+        if (Objects.nonNull(annotation)) {
+            description = annotation.value();
         }
 
         ZeusApiLogEntity apiLog = new ZeusApiLogEntity();
@@ -53,14 +72,22 @@ public class ZeusApiLogAspect {
         HttpServletRequest request = requestAttributes.getRequest();
 
         String url = request.getRequestURL().toString();
-        apiLog.setSpendTime((int)(start - end) / 1000);
+        apiLog.setSpendTime(end - start);
         apiLog.setUri(request.getRequestURI());
         apiLog.setUrl(url);
-        apiLog.setDescription(annotation.value());
+        apiLog.setDescription(description);
 
         logger.info("{}", apiLog);
 
         return result;
+    }
 
+    private boolean isPackageOrSubpackage(Set<String> packageSet, String packageName) {
+        for (String pkg : packageSet) {
+            if (packageName.startsWith(pkg + ".") || packageName.equals(pkg)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
